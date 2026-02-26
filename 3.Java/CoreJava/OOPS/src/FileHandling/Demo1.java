c40775712bc5db4fadba60b0d4c421f89b512cae
nkoushik30 <koushiknelluri827@gmail.com>
2026-02-26 16:59:31 +0530

    file handling

--- 3.Java/CoreJava/OOPS/src/FileHandling/Demo1.java
+++ 3.Java/CoreJava/OOPS/src/FileHandling/Demo1.java
@@ -1,11 +1,31 @@
 package FileHandling;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Demo1 {
 
 	public static void main(String[] args) {
 		// file handling concepts
 		
+		FileWriter writer = null;
 		
+		try{
+			writer = new FileWriter("Myfile.txt", true);
+			writer.write("Line-3 \n");
+		}catch(IOException ie){
+			ie.printStackTrace();
+		}
+		finally{
+			try{
+				if(writer!= null){
+					writer.close();
+				}	
+			}catch(IOException ie){
+				ie.printStackTrace();
+			}
+		}
+		System.out.println(":done writing:");
 
 	}
 
---
+++ 3.Java/CoreJava/OOPS/src/FileHandling/Demo2.java
@@ -0,0 +1,10 @@
+package FileHandling;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// data entry using do while 
+
+	}
+
+}
